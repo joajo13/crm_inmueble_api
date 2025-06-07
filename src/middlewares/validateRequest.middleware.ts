abc01bac947b226cc8b0e162365b47f5bc72fd3d
juan <juanbejamingiupponi@gmail.com>
2025-06-07 20:34:12 -0300

    :bug: fix: Remove debug logs from validateRequest middleware

--- src/middlewares/validateRequest.middleware.ts
+++ src/middlewares/validateRequest.middleware.ts
@@ -6,11 +6,6 @@ type RequestLocation = 'body' | 'params' | 'query';
 
 const validateRequest = (schema: ZodSchema<any>, location: RequestLocation = 'body') => 
   (req: Request, res: Response, next: NextFunction) => {
-
-    console.log("location", location);
-    console.log("schema", schema);
-    console.log("req", req);
-    console.log("req.body", req.body);
     try {
       const data = location === 'body' ? req.body : location === 'params' ? req.params : req.query;
       console.log("data", data);
